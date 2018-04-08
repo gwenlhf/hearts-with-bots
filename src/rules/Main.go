e3d248ccab10768eaebc89a31b0000343dda7f18
Ethan David <ead900@gmail.com>
2018-04-08 05:00:47 -0400

    this is not good

--- src/rules/Main.go
+++ src/rules/Main.go
@@ -2,10 +2,38 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"encoding/json"
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:    4096,
+	WriteBufferSize:   4096,
+	EnableCompression: true,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func webSocketHandler (w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer c.Close()
+	c.WriteMessage(websocket.TextMessage, []byte("it worked?"))
+	return
+}
 
 func main() {
 	game := NewGame()
 	fmt.Print(json.Marshal(game))
+	http.HandleFunc("/ws", webSocketHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("shit: ", err)
+	}
 }
